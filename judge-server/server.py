04fa9622a7ca0ad470bbb77c73ea7696ef4d5b83
OmkarKovvali <okovvali5@gmail.com>
2025-12-15 22:40:31 -0500

    added openai client

--- judge-server/server.py
+++ judge-server/server.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 load_dotenv()
 mcp = FastMCP("Manual Overlord Judge")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 class Judge: 
